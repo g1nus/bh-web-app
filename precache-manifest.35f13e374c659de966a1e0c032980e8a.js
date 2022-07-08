df38ac9edb9c2cc6ca7f0e8939db57a8d5896537
Giuliano Andronic <giuliano.andronic@studenti.unitn.it>
2022-07-08 17:17:21 +0200

    Updates

---
+++ precache-manifest.35f13e374c659de966a1e0c032980e8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f788e6645a88c61b0149",
+    "url": "/bh-web-app/static/js/main.f788e664.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/bh-web-app/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "2083978c0ad0986d54f3",
+    "url": "/bh-web-app/static/js/2.2083978c.chunk.js"
+  },
+  {
+    "revision": "ee9da0b4ffb012b7aca8",
+    "url": "/bh-web-app/static/js/runtime~main.ee9da0b4.js"
+  },
+  {
+    "revision": "ceb976770a3f4af63d84a63ac22845fe",
+    "url": "/bh-web-app/index.html"
+  }
+];
